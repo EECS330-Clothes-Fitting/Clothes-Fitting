8616a334f121c56f4aa07442662580e0a2d1a95d
NinaCong <nina.cong@outlook.com>
2019-11-27 16:12:57 -0600

    Nina P8
    - Make some fields "required"
    - User feedback after saving changes
    - Implemented the imperial/metirc toggle
    - Make gender optional+add I don't wish to answer

--- main.js
+++ main.js
@@ -23,11 +23,51 @@ function setStar(item, rating) {
 }
 
 function openTryOn(){
-    alert("You are going to try on clothes! (not implemented)")
+    alert("Not implemented")
 }
 
+function validateForm(){
+    var x = document.getElementById("name").value;
+    var y = document.getElementById("h").value;
+    var z = document.getElementById("w").value;
+    if (x == ""|| y == "" || z == "") {
+        alert("Please fill out all the required fields first");
+        return false;
+    }
+    return true
+}
+
+
 function saveChanges(){
-    alert("You are saving your changes! (not implemented)")
+    // alert("Yay!!😄🙌 The changes were successfully saved! Can't wait to see you in those cute clothes!✨")
+    if (validateForm()){
+        alert("Yay!!😄🙌 The changes were successfully saved! Can't wait to see you in those cute clothes!✨")
+    }
+}
+
+function unitSwitch(){
+    var h = document.getElementById("h_label");
+    var w = document.getElementById("w_label");
+    var c = document.getElementById("c_label");
+    var wa = document.getElementById("wa_label");
+    var b = document.getElementById("b_label");
+    var s = document.getElementById("s_label");
+    if (h.innerHTML === "Height* (cm)"){
+        h.innerHTML = "Height* (inch)";
+        w.innerHTML = "Weight* (lbs)"
+        c.innerHTML = "Chest (inch)"
+        wa.innerHTML = "Waist (inch)"
+        b.innerHTML = "Hip (inch)"
+        s.innerHTML = "Sleeve length (inch)"
+    }else{
+        h.innerHTML = "Height* (cm)"
+        w.innerHTML = "Weight* (kg)"
+        c.innerHTML = "Chest (cm)"
+        wa.innerHTML = "Waist (cm)"
+        b.innerHTML = "Hip (cm)"
+        s.innerHTML = "Sleeve length (cm)"
+    }
+    
 }
 
 init();
